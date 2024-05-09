3eb26bf8b773cb15e8ba887f14e4640f6a9c6f9e
tonycq <anthonycq2020@gmail.com>
2024-05-08 20:02:46 -0400

    feature/Login-Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-
+import React, { useState, useEffect } from 'react';
+import Home from './views/home'
+import Login from './views/login'
+import getState from './store/flux'
+import Register from './views/Register'
 import './App.css'
 import injectContext from './store/appContext'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-import Home from './views/home'
 import ProductPage from './views/product-page';
 
 function App() {
 
 
   return (
-    <BrowserRouter>
-      <>
-       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/product-page" element={<ProductPage/>}/>
-        <Route path="/product-page" element={<ProductPage/>}/>
-       </Routes>
-      </>
-    </BrowserRouter>
+
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product-page" element={<ProductPage />} />
+          <Route path="/product-page" element={<ProductPage />} />
+          <Route path={"login"} element={<Login />} />
+          <Route path={"register"} element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+   
   )
 }
 
