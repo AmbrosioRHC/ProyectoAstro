17a4aa6ca40e10078b3df711960f668b8fb02d77
Ambrosio <ambrosio.rhc@gmail.com>
2024-05-20 21:26:10 -0400

    corrigiendo loop de fetch

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,26 +4,27 @@ const Navbar = () => {
     <div className="navbarCustom">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src="../src/assets/img-logo/logo-S.png" className="navbar-logo"/>
-          </a>
+        <a className="navbar-brand" href="http://localhost:5173">
+  <img src="../src/assets/img-logo/logo-S.png" className="navbar-logo" alt="Logo" />
+</a>
+
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="http://localhost:5173">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Product page</a>
+                <a className="nav-link" href="http://localhost:5173/product-list">Product page</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" aria-disabled="true">Disabled</a>
               </li>
             </ul>
             <form className="d-flex ms-auto" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button type="button" className="btn btn-primary">Carrito</button>
             </form>
           </div>
