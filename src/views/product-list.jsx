78edfdf0520502dc14ad27445735ec202455f653
tonycq <anthonycq2020@gmail.com>
2024-06-12 17:11:46 -0400

    actua

--- src/views/product-list.jsx
+++ src/views/product-list.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import ProductCollection from "../components/productCollection";
+import './styles/LoginStyle.css';
 
 const ProductList = () => {
     const { actions, store } = useContext(Context); 
@@ -14,14 +15,23 @@ const ProductList = () => {
 
     return (
         <>
-            <div className="shopping-cart-container">
-                <div className="cart-items-container">
-                    <h2 className="cart-heading">Productos Disponibles</h2>
-                    <div className="container mt-5">
-                        <ProductCollection products={products} />
-                    </div>
+             <div className="shopping-cart-container">
+            <div className="cart-items-container">
+                <h2 className="cart-heading">Productos Disponibles</h2>
+                <div className="product-grid">
+                    {products.map(product => (
+                        <div className="card" key={product.id}>
+                            <img src={product.image} alt={product.name} className="card-img" />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <p className="card-text">{product.price}</p>
+                                <button className="btn btn-danger">Ver más</button>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
+        </div>
         </>
     );
 };
